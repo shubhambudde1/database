7307c0672000ea8bd27283224b6969fd66a6bcc3
shubhambudde1 <shubhambudde.github@gmail.com>
2023-11-30 11:45:11 -0800

    medicine pricipcetion system almost complet

---
+++ medicine preciption system/index.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql2');
+
+// create a new MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Latur@1059$',
+  database: 'health'
+});
+
+// connect to the MySQL database
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database:', error);
+  } else {
+    console.log('Connected to MySQL database!');
+
+    // retrieve all data from the "city" table
+    connection.query('SELECT * FROM health_data', (queryError, results) => {
+      if (queryError) {
+        console.error('Error executing query:', queryError);
+      } else {
+        // log the retrieved data
+        console.log('Retrieved data:', results);
+      }
+
+      // close the MySQL connection after retrieving data
+      connection.end();
+    });
+  }
+});
--- medicine preciption system/script.js
+++ medicine preciption system/script.js
@@ -1,8 +1,67 @@
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
+const port = 3000;
+app.use(express.static(__dirname));
+
+
+// MySQL database connection configuration
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Latur@1059$',
+  database: 'health',
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+// Set up your routes
+
+// Parse JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files (for example, your HTML and CSS files)
+app.use(express.static('public'));
+// get request
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
-app.listen(3000, function () {
-  console.log("Server is running on http://localhost:3000");
+
+// Handle form submission
+app.post('/submitForm', (req, res) => {
+  const { name, age, dateSigned, disease, medicine } = req.body;
+
+  // Insert data into the database
+  const insertQuery = 'INSERT INTO health_data (name, age, dateSigned, disease, medicine) VALUES (?, ?, ?, ?, ?)';
+  db.query(insertQuery, [name, age, dateSigned, disease, medicine], (err, results) => {
+    if (err) {
+      console.error('Database insertion error:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      console.log('Data inserted into the database');
+      res.status(200).send('Form submitted successfully');
+    }
+  });
 });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
